17b104a9c8d56f520568e22621705a262961a45f
yukapril <i@yukapril.com>
2018-10-13 22:42:49 +0800

    add

---
+++ rxjs/src/main.js
@@ -0,0 +1,2 @@
+const source$ = Rx.Observable.of(1, 2, 3)
+source$.subscribe(console.log)
---
+++ vue-cli3/vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  productionSourceMap: false,
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/index/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Index Page',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    // when using the entry-only string format,
+    // template is inferred to be `public/subpage.html`
+    // and falls back to `public/index.html` if not found.
+    // Output filename is inferred to be `subpage.html`.
+    page1: 'src/page1/main.js'
+  }
+}
